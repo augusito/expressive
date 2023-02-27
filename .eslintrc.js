7bfb479e4d9a241e88a3c622abe0f17c57bc58e5
augusito <kaugustus15@gmail.com>
2023-02-27 13:22:00 +0300

    chore: add dev dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'jest'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { args: 'none' }],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-types': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js', '**/*.test.ts'],
+      extends: ['plugin:jest/recommended'],
+      rules: {
+        'jest/no-disabled-tests': 'off',
+        'jest/no-conditional-expect': 'off',
+      },
+    },
+  ],
+};
