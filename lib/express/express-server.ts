a71997d6325ac1769fb949d3ca5e9c0e4e4e8872
augusito <kaugustus15@gmail.com>
2023-02-27 16:29:40 +0300

    feat: add http server

--- lib/express/express-server.ts
+++ lib/express/express-server.ts
@@ -1,9 +1,48 @@
 import * as express from 'express';
+import * as http from 'http';
+import * as https from 'https';
+import type { Server } from 'net';
 
 export class ExpressServer {
+  private httpServer: http.Server | https.Server;
+
   constructor(private readonly instance: express.Application) {}
 
   getInstance(): express.Application {
     return this.instance;
+  }
+
+  public getHttpServer(): http.Server | https.Server {
+    return this.httpServer;
+  }
+
+  public listen(port: string | number, callback?: () => void): Server;
+  public listen(
+    port: string | number,
+    hostname: string,
+    callback?: () => void,
+  ): Server;
+  public listen(port: any, ...args: any[]): Server {
+    return this.httpServer.listen(port, ...args);
+  }
+
+  public initHttpServer(options: any = {}): void {
+    const isHttpsEnabled = options && options.httpsOptions;
+    if (isHttpsEnabled) {
+      this.httpServer = https.createServer(
+        options.httpsOptions,
+        this.getInstance(),
+      );
+      return;
+    } else {
+      this.httpServer = http.createServer(this.getInstance());
+    }
+  }
+
+  public close() {
+    if (!this.httpServer) {
+      return undefined;
+    }
+    return new Promise((resolve) => this.httpServer.close(resolve));
   }
 }
