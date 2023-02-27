2b1e3bd9f9201b864e21655f27cb41efa79e8f05
augusito <kaugustus15@gmail.com>
2023-02-27 15:50:26 +0300

    refactor: rename server to expressApp

--- lib/express/__tests__/integration/express-instance.test.ts
+++ lib/express/__tests__/integration/express-instance.test.ts
@@ -3,18 +3,18 @@ import * as request from 'supertest';
 import { ExpressServer } from '../../express-server';
 
 describe('ExpressServer (express instance)', () => {
-  let server: express.Application;
+  let expressApp: express.Application;
 
   beforeEach(async () => {
     const adapter = new ExpressServer(express());
-    server = adapter.getInstance();
+    expressApp = adapter.getInstance();
   });
 
   it(`/GET`, () => {
-    server.get('/hello', (req: express.Request, res: express.Response) => {
+    expressApp.get('/hello', (req: express.Request, res: express.Response) => {
       return res.send('Hello world!');
     });
 
-    return request(server).get('/hello').expect(200).expect('Hello world!');
+    return request(expressApp).get('/hello').expect(200).expect('Hello world!');
   });
 });
