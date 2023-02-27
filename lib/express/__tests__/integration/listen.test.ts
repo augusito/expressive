f083be723327802280aea78b3b320f141cbdbe36
augusito <kaugustus15@gmail.com>
2023-02-27 16:59:04 +0300

    test: add express server listen tests

---
+++ lib/express/__tests__/integration/listen.test.ts
@@ -0,0 +1,20 @@
+import * as express from 'express';
+import { ExpressServer } from '../../express-server';
+
+describe('ExpressServer (listen)', () => {
+  let expressServer: ExpressServer;
+
+  beforeEach(() => {
+    expressServer = new ExpressServer(express());
+    expressServer.initHttpServer();
+  });
+
+  afterEach(async () => {
+    await expressServer.close();
+  });
+
+  it('should resolve with httpServer on success', () => {
+    const response = expressServer.listen(3000);
+    expect(response).toEqual(expressServer.getHttpServer());
+  });
+});
