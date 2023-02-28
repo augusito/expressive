31efbce68f43bde50ae1b4ce07d7f1b1d7fbe92f
augusito <kaugustus15@gmail.com>
2023-02-28 16:43:03 +0300

    fix: pipeline typo

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { Application } from '../lib/core/application';
 import { container } from './container';
-import { definePepiline } from './pipeline';
+import { definePipeline } from './pipeline';
 import { defineRoutes } from './routes';
 
 async function main() {
   const app = container.get<Application>(Application.name);
   await app.listen(3000);
-  definePepiline(app.getServer(), container);
+  definePipeline(app.getServer(), container);
   defineRoutes(app.getServer(), container);
   console.log(`Listening on port: ${await app.getUrl()}`);
 }
--- src/pipeline.ts
+++ src/pipeline.ts
@@ -8,7 +8,7 @@ import { getBodyParserOptions } from '../lib/common/get-body-parser-options';
 import { IContainer } from '../lib/container/types';
 import { ExpressServer } from '../lib/express/express-server';
 
-export function definePepiline(server: ExpressServer, container: IContainer) {
+export function definePipeline(server: ExpressServer, container: IContainer) {
   const config: any = container.has('config') ? container.get('config') : {};
   const bodyParserOptions = config.bodyParser?.options ?? {};
   const rawBody = !!bodyParserOptions.rawBody;
